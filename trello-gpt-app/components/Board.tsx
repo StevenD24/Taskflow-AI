c8c7e249db15e24e073db18e81b68de95e1b2fd3
Steven Duong <stevenduongx@gmail.com>
2023-05-28 17:53:53 -0600

    update files

--- trello-gpt-app/components/Board.tsx
+++ trello-gpt-app/components/Board.tsx
@@ -2,7 +2,7 @@
 
 import { useBoardStore } from '@/store/BoardStore';
 import { useEffect } from 'react';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd';
 
 function Board() {
 
@@ -15,19 +15,31 @@ function Board() {
     getBoard();
   }, [getBoard])
 
+  const handleOnDragEnd = (result: DropResult) => {};
+
   console.log(board);
 
   return (
-    <h1>Hello</h1>
-    // <DragDropContext>
-    //     <Droppable droppableId="board" direction='horizontal' type='column'>
-    //         {(provided) => (
-    //             <div>
-    //                 {/* rendering all the columns */}
-    //             </div>
-    //         )}
-    //     </Droppable>
-    // </DragDropContext>
+    <DragDropContext onDragEnd={e => handleOnDragEnd}>
+        <Droppable droppableId="board" direction='horizontal' type='column'>
+            {(provided) => (
+                <div
+                className='grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto'
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                >{
+                    Array.from(board.columns.entries()).map(([id, column], index) => (
+                        <Column 
+                            key={id}
+                            id={id}
+                            todos={column.todos}
+                            index={index}
+                        />
+                    ))
+                }</div>
+            )}
+        </Droppable>
+    </DragDropContext>
   );
 }
 
