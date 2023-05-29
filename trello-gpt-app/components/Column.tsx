38d8d8525e0ec7acb5485a3c2c097beb5c2bc7ca
Steven Duong <stevenduongx@gmail.com>
2023-05-28 18:43:50 -0600

    Update Column.tsx

--- trello-gpt-app/components/Column.tsx
+++ trello-gpt-app/components/Column.tsx
@@ -6,6 +6,14 @@ type Props = {
     index: number
 }
 
+const idToColumnText: {
+    [key in TypedColumn]: string;
+} = {
+    "todo": "To do",
+    "inprogress": "In Progress",
+    "done": "Done",
+}
+
 function Column({id, todos, index}: Props) {
   return (
     <Draggable draggableId={id} index={index}>
@@ -25,7 +33,7 @@ function Column({id, todos, index}: Props) {
                                 snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
                             }`}
                         >
-                            <h2>{id}</h2>
+                            <h2>{idToColumnText[id]}</h2>
                         </div>
                     )}
                 </Droppable>
