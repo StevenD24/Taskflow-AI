56b8b016a85272078fe008bba3f1571a08c0ce59
Steven Duong <stevenduongx@gmail.com>
2023-05-28 21:54:06 -0600

    update files

--- trello-gpt-app/components/Board.tsx
+++ trello-gpt-app/components/Board.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 
 import { useBoardStore } from '@/store/BoardStore';
 import { useEffect } from 'react';
@@ -6,44 +6,114 @@ import { DragDropContext, Droppable, DropResult } from '@hello-pangea/dnd';
 import Column from './Column';
 
 function Board() {
-
-  const [board, getBoard] = useBoardStore((state) => [
+  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore((state) => [
     state.board,
-    state.getBoard]);  
+    state.getBoard,
+    state.setBoardState,
+    state.updateTodoInDB,
+  ]);
 
   useEffect(() => {
     // get board
     getBoard();
-  }, [getBoard])
+  }, [getBoard]);
 
   const handleOnDragEnd = (result: DropResult) => {
-    
+    const { destination, source, type } = result;
+
+    // Check if user dragged card outside of board
+    if (!destination) return;
+
+    // Handle column drag
+    if (type === 'column') {
+      const columnOrder = Array.from(board.columns.keys());
+      const newColumnOrder = [...columnOrder];
+      newColumnOrder.splice(source.index, 1);
+      newColumnOrder.splice(destination.index, 0, ...columnOrder.slice(source.index, source.index + 1));
+
+      const newColumns = new Map<TypedColumn, Column | undefined>(
+        newColumnOrder.map((columnId) => [columnId, board.columns.get(columnId)]),
+      );
+
+      setBoardState({
+        ...board,
+        columns: newColumns,
+      });
+    }
+
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const finishColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1]!.todos,
+    };
+
+    const finishCol: Column = {
+      id: finishColIndex[0],
+      todos: finishColIndex[1]!.todos,
+    };
+
+    if (!startCol || !finishCol) return;
+
+    if (source.index === destination.index && startCol === finishCol) return;
+
+    const newTodos = startCol.todos;
+    const [todoMoved] = newTodos.splice(source.index, 1);
+
+    if (startCol.id === finishCol.id) {
+      // same column task drag
+      newTodos.splice(destination.index, 0, todoMoved);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+
+      const newColumns = new Map(board.columns);
+      newColumns.set(startCol.id, newCol);
+
+      setBoardState({ ...board, columns: newColumns });
+    } else {
+      // dragging to another column
+      const finishTodos = Array.from(finishCol.todos);
+      finishTodos.splice(destination.index, 0, todoMoved);
+
+      const newColumns = new Map(board.columns);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+
+      newColumns.set(startCol.id, newCol);
+      newColumns.set(finishCol.id, {
+        id: finishCol.id,
+        todos: finishTodos,
+      });
+
+      // update in DB
+      updateTodoInDB(todoMoved, finishCol.id);
+
+      setBoardState({ ...board, columns: newColumns });
+    }
   };
 
   console.log(board);
 
   return (
-    <DragDropContext onDragEnd={e => handleOnDragEnd}>
-        <Droppable droppableId="board" direction='horizontal' type='column'>
-            {(provided) => (
-                <div
-                className='grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto'
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                >{
-                    Array.from(board.columns.entries()).map(([id, column], index) => (
-                        <Column 
-                            key={id}
-                            id={id}
-                            todos={column.todos}
-                            index={index}
-                        />
-                    ))
-                }</div>
-            )}
-        </Droppable>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto" {...provided.droppableProps} ref={provided.innerRef}>
+            {Array.from(board.columns.entries()).map(([id, column], index) => (
+              <Column key={id} id={id} todos={column?.todos || []} index={index} />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
     </DragDropContext>
   );
 }
 
-export default Board
+export default Board;
--- trello-gpt-app/components/Column.tsx
+++ trello-gpt-app/components/Column.tsx
@@ -1,4 +1,7 @@
+import { useBoardStore } from "@/store/BoardStore";
 import { Draggable, Droppable } from "@hello-pangea/dnd";
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
+import TodoCard from "./TodoCard";
 
 type Props = {
     id: TypedColumn,
@@ -15,6 +18,8 @@ const idToColumnText: {
 }
 
 function Column({id, todos, index}: Props) {
+    const [searchString] = useBoardStore((state) => [state.searchString]);
+
   return (
     <Draggable draggableId={id} index={index}>
         {(provided) => (
@@ -30,10 +35,52 @@ function Column({id, todos, index}: Props) {
                             {...provided.droppableProps}
                             ref={provided.innerRef}
                             className={`pb-2 p-2 rounded-2xl shadow-sm ${
-                                snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
+                                snapshot.isDraggingOver ? "bg-green-100/50" : "bg-white/50"
                             }`}
                         >
-                            <h2>{idToColumnText[id]}</h2>
+                            <h2 className="flex justify-between font-bold text-xl p-2">
+                                {idToColumnText[id]}
+                                <span className="text-gray-500 bg-gray-200 rounded-full 
+                                px-2 py-1 text-sm font-normal">
+                                    {!searchString ? todos.length : todos
+                                        .filter(todo => todo.title.toLowerCase().includes(searchString
+                                        .toLowerCase())).length}
+                                </span>
+                            </h2>
+
+                            <div className="space-y-2">
+                                {todos.map((todo, index) => {
+                                    
+                                    if (searchString && !todo.title.toLocaleLowerCase()
+                                        .includes(searchString.toLowerCase())) return null;
+
+                                    return (
+                                    <Draggable
+                                        key={todo.$id}
+                                        draggableId={todo.$id}
+                                        index={index}
+                                    >
+                                      {(provided) => (
+                                        <TodoCard 
+                                            todo={todo}
+                                            index={index}
+                                            id={id}
+                                            innerRef={provided.innerRef}
+                                            draggableProps={provided.draggableProps}
+                                            dragHandleProps={provided.dragHandleProps}
+                                        />
+                                      )}
+                                    </Draggable>
+                                )})}
+                                {provided.placeholder}
+                                <div className="flex items-end justify-end p-2">
+                                    <button className="text-green-500/90 hover:text-green-600">
+                                        <PlusCircleIcon 
+                                            className="h-10 w-10"
+                                        />
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     )}
                 </Droppable>
